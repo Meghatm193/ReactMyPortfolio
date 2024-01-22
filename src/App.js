9e3447579874a1e95c2b99106614d810ee78ebf5
Megha <meghamokashi193@gmail.com>
2024-01-22 22:50:11 +0530

    path added in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import About from "./routes/About";
 import Contact from "./routes/Contact";
 import Project from "./routes/Project";
 
-import { Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/project" element={<Project />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
